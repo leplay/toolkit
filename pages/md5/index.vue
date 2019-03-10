6342ed16a5166e19c74af68a016a4e711dced78a
leplay <leqcany@gmail.com>
2019-03-11 00:13:37 +0800

    md5 generator

---
+++ pages/md5/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="tool container">
+    <h3 class="title has-text-centered">{{ app.name }}</h3>
+    <p class="description has-text-centered">{{ app.description }}</p>
+
+    <div class="tool-box columns-two">
+      <InputEditor class="column" />
+      <OutputEditor class="column" :data="dataMap" :readonly="true" />
+    </div>
+  </div>
+</template>
+
+<script>
+import md5 from 'md5'
+import meta from '@/config/meta'
+import InputEditor from '@/components/InputEditor'
+import OutputEditor from '@/components/OutputEditor'
+const app = meta('md5')
+
+export default {
+  components: { InputEditor, OutputEditor },
+  head () {
+    return app.head
+  },
+  data () {
+    return {
+      app
+    }
+  },
+  computed: {
+    dataMap () {
+      return [
+        {
+          label: 'md5',
+          data: md5(this.$store.state.input)
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* inherrit from layout/default.vue */
+</style>
